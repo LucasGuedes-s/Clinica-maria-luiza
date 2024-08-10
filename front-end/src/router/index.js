00aab1b7ade0af177bd6d7d33cb36654cc364b42
Lucas Guedes <lucasguedes2908@gmail.com>
2024-08-10 14:56:54 -0300

    Corrigindo caminho no sideBar e adicionando a rota ao index.js

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -33,6 +33,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/ProfissionaisView.vue')
   },
   {
+    path: '/pacientes',
+    name: 'pacientes',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/PacientesView.vue')
+  },
+  {
     path: '/unauthorized',
     name: 'naoAutorizado',
     // route level code-splitting
