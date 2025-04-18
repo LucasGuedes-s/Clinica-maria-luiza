11e8971608edceb848d874d47763bef82bc7d5a1
João Paulo <joaopaulosv068@gmail.com>
2025-04-18 12:41:41 -0300

    Implementação de Modal para edição de agendamentos no Front-end

--- front-end/src/views/DashboardView.vue
+++ front-end/src/views/DashboardView.vue
@@ -7,9 +7,12 @@
             <h1>Bem-vindo(a)!</h1>
         </div>
         <div class="contagem_dashboard" v-if="permissao === 1">
-            <p>Total de consultas: <strong> <span class="value_totalconsultas"> {{ total.totalConsultasAba + total.totalConsultas}} </span></strong> </p>
-            <p>Total de consultas de rotina: <strong> <span class="value_totalconsultasrotina">{{ total.totalConsultas }} </span></strong> </p>
-            <p>Total de consultas ABA: <strong> <span class="value_totalconsultasaba"> {{ total.totalConsultasAba }} </span></strong> </p>
+            <p>Total de consultas: <strong> <span class="value_totalconsultas"> {{ total.totalConsultasAba +
+                        total.totalConsultas}} </span></strong> </p>
+            <p>Total de consultas de rotina: <strong> <span class="value_totalconsultasrotina">{{ total.totalConsultas
+                        }} </span></strong> </p>
+            <p>Total de consultas ABA: <strong> <span class="value_totalconsultasaba"> {{ total.totalConsultasAba }}
+                    </span></strong> </p>
         </div>
         <div class="container_dashboard">
             <img :src="imageUrl" alt="Foto do Usuário" @error="onImageError" />
@@ -17,7 +20,7 @@
                 <p>Nome: {{ nome }}</p>
                 <p>E-mail: {{ email }}</p>
                 <p>Telefone: {{ telefone }}</p>
-                    <!-- <router-link to="/alterarsenha"><button class="alterar_senha_btn" click="teste">Alterar
+                <!-- <router-link to="/alterarsenha"><button class="alterar_senha_btn" click="teste">Alterar
                             Senha</button></router-link>-->
                 <router-link to="/realizarpagamento"><button class="realizarpagamento_btn" click="">Realizar
                         Pagamento</button></router-link>
@@ -49,10 +52,42 @@
                     <button class="btn-concluido"
                         @click="enviarNotificacao(agenda.paciente.telefone, agenda.paciente.nome, agenda.dataFormatada, agenda.horaFormatada)">Enviar
                         notificação</button>
+                    <button class="btn_editaragendamento" @click="abrirModal(agenda)">Editar Consulta</button>
                 </div>
 
             </div>
         </div>
+
+        <!-- Modal de Edição -->
+        <div v-if="showModal" class="modal-overlay">
+            <div class="modal-content">
+                <h2>Editar Agendamento</h2>
+                <label for="agendamento_especialidade">Agendamento:</label>
+                <input type="text" id="agendamento_especialidade" v-model="agendaEditada.agendamento">
+
+
+                <label for="paciente-nome">Profissional:</label>
+                <input type="text" id="paciente-nome" v-model="agendaEditada.profissional.nome" readonly>
+
+
+                <label for="paciente-nome">Nome do Paciente:</label>
+                <input type="text" id="paciente-nome" v-model="agendaEditada.paciente.nome" readonly>
+
+                <label for="editar-data">Data:</label>
+                <input type="date" id="editar-data" v-model="agendaEditada.data" />
+
+                <label for="editar-hora">Hora:</label>
+                <input type="time" id="editar-hora" v-model="agendaEditada.hora" />
+
+                <div class="modal-buttons">
+                    <button @click="salvarEdicao">Salvar</button>
+                    <button @click="deletarAgendamento">Deletar</button>
+                    <button @click="fecharModal">Cancelar</button>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
@@ -183,7 +218,8 @@ h2 {
     /* Inclui o padding e borda na largura total */
 }
 
-.btn-concluido {
+.btn-concluido,
+.btn_editaragendamento{
     margin-top: 20px;
     padding: 10px 20px;
     border-radius: 4px;
@@ -210,6 +246,73 @@ h2 {
 .value_totalconsultasrotina,
 .value_totalconsultasaba {
     color: #84E7FF;
+}
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.modal-content h2 {
+    color: #84E7FF;
+    margin-bottom: 2px;
+    font-size: 30px;
+    margin-top: 2px;
+}
+
+.modal-content {
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    border: 2px solid #84E7FF;
+    box-shadow: 0 4px 10px -1px rgba(0, 0, 0, 0.10);
+}
+
+.modal-content input,
+.modal-content textarea {
+    padding: 10px;
+    border: 1px solid #D9D9D9;
+    border-radius: 4px;
+    background-color: white;
+    font-size: 16px;
+    color: #7E7E7E;
+    width: 100%;
+    box-sizing: border-box;
+    outline: none;
+    box-shadow: none;
+    font-family: 'Montserrat', sans-serif;
+    line-height: 1.5;
+    text-align: justify;
+    resize: none;
+}
+
+.modal-buttons {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    gap: 20px;
+}
+
+.modal-buttons button {
+    flex: 1;
+    padding: 10px 20px;
+    border-radius: 4px;
+    background-color: #F5F5F5;
+    color: #7E7E7E;
+    border: 1px solid #D9D9D9;
+    font-size: 14px;
+    cursor: pointer;
+    font-family: 'Montserrat', sans-serif;
 }
 @media (max-width: 768px) {
     .main_content_dashboard {
@@ -351,7 +454,9 @@ export default {
             email: null,
             permissao: null,
             telefone: null,
-            imageUrl: null
+            imageUrl: null,        
+            agendaEditada: {},
+            showModal: false,
         }
     },
     methods: {
@@ -382,6 +487,128 @@ export default {
             }).catch(error => {
                 console.log(error)
             })
+        },
+        abrirModal(agenda) {
+            this.agendaEditada = {...agenda,
+            };
+            this.showModal = true;
+        },
+        fecharModal() {
+            this.showModal = false; // Fecha o modal
+        },
+        async salvarEdicao() {
+            try {
+                const token = this.store.token;
+
+                // Verifica se data e hora estão preenchidas
+                const data = this.agendaEditada.data;
+                const hora = this.agendaEditada.hora;
+
+                if (!data || !hora) {
+                    Swal.fire({
+                        icon: 'warning',
+                        title: 'Campos obrigatórios',
+                        text: 'Por favor, preencha data e hora corretamente.'
+                    });
+                    return;
+                }
+
+                // Junta data e hora num formato ISO válido
+                const dataStr = `${data}T${hora}:00`;
+                const dataHora = new Date(dataStr);
+
+                if (isNaN(dataHora.getTime())) {
+                    throw new Error('Data e hora inválidas');
+                }
+
+                const dataISO = dataHora.toISOString();
+
+                const response = await Axios.put(`http://localhost:3000/atualizar/agendamento`, {
+                    agenda: {
+                        id: this.agendaEditada.id,
+                        dadosAtualizados: {
+                            agendamento: this.agendaEditada.agendamento,
+                            profissional: this.agendaEditada.profissional.email,
+                            paciente: this.agendaEditada.paciente.cpf,
+                            data: dataISO,
+                        },
+                    }
+                }, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    }
+                });
+
+                if (response.status === 200) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Agendamento Atualizado!',
+                        text: 'O agendamento foi atualizado com sucesso.',
+                        timer: 2000,
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    });
+                    this.getAgendamentos();
+                    this.fecharModal();
+                }
+
+            } catch (error) {
+                console.error('Erro ao salvar a edição:', error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Erro',
+                    text: 'Houve um erro ao atualizar o agendamento. Tente novamente.',
+                });
+            }
+        },
+        async deletarAgendamento() {
+            try {
+                const confirmacao = await Swal.fire({
+                    title: 'Tem certeza?',
+                    text: "Você deseja deletar este agendamento? Esta ação não pode ser desfeita.",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#3085d6',
+                    confirmButtonText: 'Sim, deletar!',
+                    cancelButtonText: 'Cancelar'
+                });
+
+                if (!confirmacao.isConfirmed) return;
+
+                const token = this.store.token;
+
+                const response = await Axios.put('http://localhost:3000/atualizar/agendamento', {
+                    agenda: {
+                        id: this.agendaEditada.id,
+                        deletar: true
+                    }
+                }, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Deletado!',
+                    text: response.data.mensagem || 'Agendamento deletado com sucesso.',
+                    timer: 2000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                });
+
+                this.getAgendamentos(); // Atualiza lista
+                this.fecharModal(); // Fecha o modal
+
+            } catch (error) {
+                console.error('Erro ao deletar agendamento:', error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Erro',
+                    text: 'Não foi possível deletar o agendamento. Tente novamente.'
+                });
+            }
         },
         async enviarNotificacao(telefone, paciente, data, hora) {
             Swal.fire({
